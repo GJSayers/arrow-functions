edb69e17a27f6a71b949e723d5ad929228f9b471
GJSayers <gemma.sayers1@btinternet.com>
2022-03-21 13:08:20 +0000

    Add different options for arrow function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a,b) {
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+// Curly braces needed for more complex logic
+
+const addTwoNumbers = (a,b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(4,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
-// Implicit Returns
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
-// Returning Multiple Lines
+// Implicit Returns / functions that take a single parameter 
+const saySomething = message => console.log(message);
+saySomething('Testing message!');
+
+const sayHello = () => console.log('hello');
+sayHello();
+
+// Returning Multiple Lines if you need to wrap multiple lines of code you will need parenthesis
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>` 
+)
+console.log(returnMultipleLines());
